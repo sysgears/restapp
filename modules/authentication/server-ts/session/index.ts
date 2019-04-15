f83b06894fabe0d7096628c0da28edeebcc8f9df
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-04-15 14:54:21 +0300

    Moved initPassport to index file and rename to onAppCreate

--- modules/authentication/server-ts/session/index.ts
+++ modules/authentication/server-ts/session/index.ts
@@ -1,13 +1,12 @@
 import { Express } from 'express';
+import { Strategy } from 'passport-local';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import passport from 'passport';
 
 import ServerModule from '@restapp/module-server-ts';
-import initPassport from './initPassport';
 
 const beforeware = (app: Express) => {
-  initPassport();
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(
@@ -22,6 +21,42 @@ const beforeware = (app: Express) => {
   app.use(passport.session());
 };
 
+const userDB: User = {
+  username: 'test-user',
+  password: '123',
+  id: 1
+};
+
+interface User {
+  username: string;
+  password: string;
+  id: number;
+}
+
+passport.serializeUser((user: User, cb) => {
+  cb(null, user.username);
+});
+
+passport.deserializeUser((username, cb) => {
+  if (username === userDB.username) {
+    return cb(null, userDB);
+  }
+  return cb(null);
+});
+
+const onAppCreate = () => {
+  passport.use(
+    'login',
+    new Strategy((username: string, password: string, done: any) => {
+      if (username !== userDB.username || password !== userDB.password) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+      return done(null, userDB);
+    })
+  );
+};
+
 export default new ServerModule({
-  beforeware: [beforeware]
+  beforeware: [beforeware],
+  onAppCreate: [onAppCreate]
 });
