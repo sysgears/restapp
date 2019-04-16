0cacf570c3d2f6b190473944008a9660b092fb18
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-04-16 19:00:13 +0300

    Add token generation

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,10 @@
 export default {
   secret: process.env.NODE_ENV === 'test' ? 'secret for tests' : process.env.AUTH_SECRET,
   session: {
-    enabled: true
+    enabled: false
   },
   jwt: {
-    enabled: false,
+    enabled: true,
     tokenExpiresIn: '1m',
     refreshTokenExpiresIn: '7d'
   }
--- modules/user/server-ts/controllers.ts
+++ modules/user/server-ts/controllers.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from 'express';
 import passport from 'passport';
+import jsonwebtoken from 'jsonwebtoken';
 
 import settings from '../../../settings';
 
 const {
-  auth: { session, jwt }
+  auth: { session, jwt, secret }
 } = settings;
 
 export const login = (req: Request, res: Response) => {
   passport.authenticate('local', { session: session.enabled }, (err, user, info) => {
     if (err || !user) {
       return res.status(400).json({
-        message: info ? info.message : 'Logindd failed',
+        message: info ? info.message : 'Login failed',
         user
       });
     }
@@ -21,7 +22,7 @@ export const login = (req: Request, res: Response) => {
         res.send(loginErr);
       }
 
-      const token = jwt.enabled ? 'create tokens' : null;
+      const token = jwt.enabled ? jsonwebtoken.sign({ user }, secret, { expiresIn: jwt.tokenExpiresIn }) : null;
 
       return res.json({ user, token });
     });
