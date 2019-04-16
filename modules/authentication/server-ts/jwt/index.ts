55f3cb286c68b5c05eff76b1a541a14948605f6f
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-04-16 19:14:26 +0300

    Add jwt strategy

--- modules/authentication/server-ts/jwt/index.ts
+++ modules/authentication/server-ts/jwt/index.ts
@@ -1,9 +1,15 @@
 import { Express } from 'express';
-import { Strategy } from 'passport-local';
+import { Strategy as LocalStratery } from 'passport-local';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 
 import ServerModule from '@restapp/module-server-ts';
+import settings from '../../../../settings';
+
+const {
+  auth: { secret }
+} = settings;
 
 const beforeware = (app: Express) => {
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,12 +32,29 @@ interface User {
 
 const onAppCreate = () => {
   passport.use(
-    new Strategy((username: string, password: string, done: any) => {
+    new LocalStratery((username: string, password: string, done: any) => {
       if (username !== userDB.username || password !== userDB.password) {
         return done(null, false, { message: 'Incorrect username or password.' });
       }
       return done(null, userDB);
     })
+  );
+
+  passport.use(
+    new JWTStrategy(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: secret
+      },
+      (jwtPayload: User, cb: any) => {
+        const { username, password } = jwtPayload;
+
+        if (username !== userDB.username || password !== userDB.password) {
+          return cb(null, false, { message: 'Incorrect username or password.' });
+        }
+        return cb(null, userDB);
+      }
+    )
   );
 };
 
