74751ec13d9fdd9e478f973a26188b88619d44d6
Ivan Isakov <washerd2008@gmail.com>
2019-04-22 17:13:09 +0300

    Add docs for Access module

--- modules/module/common/AccessModule.ts
+++ modules/module/common/AccessModule.ts
@@ -1,5 +1,8 @@
 import CommonModule, { CommonModuleShape } from './CommonModule';
 
+/**
+ * Access module which provides authentication functions
+ */
 export interface AccessModuleShape extends CommonModuleShape {
   login?: Array<() => Promise<void>>;
   logout?: Array<() => Promise<void>>;
@@ -11,7 +14,11 @@ class AccessModule extends CommonModule {
   constructor(...modules: AccessModuleShape[]) {
     super(...modules);
   }
-
+  /**
+   * authentication allows to call all methods of login and logout in authentication modules.
+   *
+   * @returns merge login or logout methods
+   */
   get authentication() {
     const doLogin = () => this.login && this.login.forEach(login => login());
     const doLogout = () => this.logout && this.logout.forEach(logout => logout());
