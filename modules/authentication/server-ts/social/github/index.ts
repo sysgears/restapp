6bdb09e777f42a3b80fb10e86091f82e08c0372b
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-05-03 14:08:54 +0300

    Update auth social github

--- modules/authentication/server-ts/social/github/index.ts
+++ modules/authentication/server-ts/social/github/index.ts
@@ -1,45 +1,53 @@
 import { Express } from 'express';
 import passport from 'passport';
 import { Strategy as GitHubStrategy } from 'passport-github';
+
+import { RestMethod } from '@restapp/module-server-ts';
+
+import { auth, onAuthenticationSuccess } from './controllers';
 import settings from '../../../../../settings';
 import AuthModule from '../AuthModule';
 
-const { clientID, clientSecret, scope, callbackURL, enabled } = settings.auth.social.github;
-
-interface AppContext {
-  social: any;
-}
-
-const middleware = (app: Express, appContext: AppContext) => {
-  if (!enabled || __TEST__) {
-    return false;
+const {
+  auth: {
+    session,
+    social: {
+      github: { clientID, clientSecret, scope, callbackURL, enabled }
+    }
   }
+} = settings;
 
+const beforeware = (app: Express) => {
   app.use(passport.initialize());
-
-  app.get('/auth/github', (req, res, next) => {
-    passport.authenticate('github', { state: req.query.expoUrl })(req, res, next);
-  });
-
-  app.get(
-    '/auth/github/callback',
-    passport.authenticate('github', { session: false, failureRedirect: '/login' }),
-    appContext.social ? appContext.social.github.onAuthenticationSuccess : undefined
-  );
 };
 
 const onAppCreate = ({ appContext }: AuthModule) => {
-  if (enabled && !__TEST__) {
-    passport.use(
-      new GitHubStrategy(
-        { clientID, clientSecret, scope, callbackURL },
-        appContext.social ? appContext.social.github.verifyCallback : undefined
-      )
-    );
-  }
+  passport.use(
+    new GitHubStrategy(
+      { clientID, clientSecret, scope, callbackURL },
+      appContext.social ? appContext.social.github.verifyCallback : undefined
+    )
+  );
 };
 
-export default new AuthModule({
-  middleware: [middleware],
-  onAppCreate: [onAppCreate]
-});
+export default (enabled && !__TEST__
+  ? new AuthModule({
+      beforeware: [beforeware],
+      onAppCreate: [onAppCreate],
+      apiRouteParams: [
+        {
+          method: RestMethod.GET,
+          route: 'auth/github',
+          controller: [auth]
+        },
+        {
+          method: RestMethod.GET,
+          route: 'auth/github/callback',
+          controller: [
+            passport.authenticate('github', { session: session.enabled, failureRedirect: '/login' }),
+            onAuthenticationSuccess
+          ]
+        }
+      ]
+    })
+  : undefined);
