667e54cccc73139f05d4a7ecd8e0ae4534b49c09
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-05-03 13:53:11 +0300

    Update auth social facebook

--- modules/authentication/server-ts/social/facebook/index.ts
+++ modules/authentication/server-ts/social/facebook/index.ts
@@ -1,45 +1,53 @@
 import { Express } from 'express';
 import passport from 'passport';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
-import settings from '../../../../../settings';
+
+import { RestMethod } from '@restapp/module-server-ts';
+
+import { auth, onAuthenticationSuccess } from './controllers';
 import AuthModule from '../AuthModule';
+import settings from '../../../../../settings';
 
-const { clientID, clientSecret, callbackURL, profileFields, enabled } = settings.auth.social.facebook;
-
-interface AppContext {
-  social: any;
-}
-
-const middleware = (app: Express, appContext: AppContext) => {
-  if (!enabled || __TEST__) {
-    return false;
+const {
+  auth: {
+    session,
+    social: {
+      facebook: { clientID, clientSecret, callbackURL, profileFields, enabled }
+    }
   }
+} = settings;
 
+const beforeware = (app: Express) => {
   app.use(passport.initialize());
-
-  app.get('/auth/facebook', (req, res, next) => {
-    passport.authenticate('facebook', { state: req.query.expoUrl })(req, res, next);
-  });
-
-  app.get(
-    '/auth/facebook/callback',
-    passport.authenticate('facebook', { session: false, failureRedirect: '/login' }),
-    appContext.social ? appContext.social.facebook.onAuthenticationSuccess : undefined
-  );
 };
 
 const onAppCreate = ({ appContext }: AuthModule) => {
-  if (enabled && !__TEST__) {
-    passport.use(
-      new FacebookStrategy(
-        { clientID, clientSecret, callbackURL, profileFields },
-        appContext.social ? appContext.social.facebook.verifyCallback : undefined
-      )
-    );
-  }
+  passport.use(
+    new FacebookStrategy(
+      { clientID, clientSecret, callbackURL, profileFields },
+      appContext.social ? appContext.social.facebook.verifyCallback : undefined
+    )
+  );
 };
 
-export default new AuthModule({
-  middleware: [middleware],
-  onAppCreate: [onAppCreate]
-});
+export default (enabled && !__TEST__
+  ? new AuthModule({
+      beforeware: [beforeware],
+      onAppCreate: [onAppCreate],
+      apiRouteParams: [
+        {
+          method: RestMethod.GET,
+          route: 'auth/facebook',
+          controller: [auth]
+        },
+        {
+          method: RestMethod.GET,
+          route: 'auth/facebook/callback',
+          controller: [
+            passport.authenticate('facebook', { session: session.enabled, failureRedirect: '/login' }),
+            onAuthenticationSuccess
+          ]
+        }
+      ]
+    })
+  : undefined);
