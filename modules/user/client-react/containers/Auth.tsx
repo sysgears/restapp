e65edd9bb83704489bdd8cee5a4fdc0409e4bc88
Ivan Isakov <washerd2008@gmail.com>
2019-05-07 08:55:39 +0300

    Add auth base components

---
+++ modules/user/client-react/containers/Auth.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Route, Redirect, RouteComponentProps } from 'react-router-dom';
+import { withLoadedUser } from './AuthBase';
+import { UserRole, User } from '..';
+import { WithUserProps } from './AuthBase';
+
+interface AuthRouteProps extends WithUserProps {
+  role?: UserRole | UserRole[];
+  redirect?: string;
+  redirectOnLoggedIn?: boolean;
+  component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+}
+
+const AuthRoute: React.ComponentType<AuthRouteProps> = withLoadedUser(
+  ({ currentUser, role, redirect = '/register', redirectOnLoggedIn, component: Component, ...rest }) => {
+    const RenderComponent: React.FunctionComponent<any> = props => {
+      // The users is not logged in
+      if (redirectOnLoggedIn && currentUser) {
+        return <Redirect to={{ pathname: redirect }} />;
+      }
+
+      return isRoleMatch(role, currentUser) ? (
+        <Component currentUser={currentUser} {...props} {...rest} />
+      ) : (
+        <Redirect to={{ pathname: redirect }} />
+      );
+    };
+
+    return <Route {...rest} render={RenderComponent} />;
+  }
+);
+
+const isRoleMatch = (role: UserRole | UserRole[], currentUser: User) => {
+  if (!role) {
+    return true;
+  }
+  return currentUser && (Array.isArray(role) ? role : [role]).includes(currentUser.role);
+};
+
+export * from './AuthBase';
+export { AuthRoute };
---
+++ modules/user/client-react/containers/AuthBase.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { RouteProps } from 'react-router';
+import { History } from 'history';
+import { connect } from 'react-redux';
+import { CURRENT_USER } from '../actions';
+import { User, UserRole } from '..';
+import { ActionType } from '../reducers';
+
+export interface WithUserProps extends RouteProps {
+  currentUser?: User;
+  currentUserLoading?: boolean;
+  refetchCurrentUser?: any;
+  children?: Element | any;
+  getCurrentUser?: () => void;
+}
+
+interface IfLoggedInComponent {
+  role?: UserRole | UserRole[];
+  currentUser?: User;
+  refetchCurrentUser?: any;
+  elseComponent?: Element | any;
+  children?: Element | any;
+}
+
+export interface WithLogoutProps extends WithUserProps {
+  logout?: () => void;
+  history?: History;
+  clearUser?: () => void;
+}
+
+const withUser = (Component: React.ComponentType<any>) => {
+  const WithUser = ({ currentUser, ...rest }: WithUserProps) => {
+    return <Component currentUser={currentUser} {...rest} />;
+  };
+  return connect(({ user: { loading, currentUser } }: any) => ({
+    currentUserLoading: loading,
+    currentUser
+  }))(WithUser);
+};
+
+const hasRole = (role: UserRole | UserRole[], currentUser: User) => {
+  return currentUser && (!role || (Array.isArray(role) ? role : [role]).indexOf(currentUser.role) >= 0) ? true : false;
+};
+
+const withLoadedUser = (Component: React.ComponentType<any>) => {
+  const WithLoadedUser = ({ currentUserLoading, ...props }: WithUserProps) => {
+    return currentUserLoading ? null : <Component {...props} />;
+  };
+
+  return withUser(WithLoadedUser);
+};
+
+const IfNotLoggedInComponent: React.FunctionComponent<IfLoggedInComponent> = ({
+  currentUser,
+  children,
+  refetchCurrentUser,
+  ...restProps
+}) =>
+  !currentUser
+    ? React.cloneElement(children, {
+        ...restProps
+      })
+    : null;
+
+const IfNotLoggedIn: React.ComponentType<IfLoggedInComponent> = withLoadedUser(IfNotLoggedInComponent);
+
+export { withUser, hasRole, withLoadedUser, IfNotLoggedIn };
