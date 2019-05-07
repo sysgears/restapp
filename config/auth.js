72b4ba94e705a650667412b8a5d98a1e2de89f35
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-05-07 15:46:36 +0300

    Add config for session and jwt

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,13 @@
 export default {
+  secret: process.env.NODE_ENV === 'test' ? 'secret for tests' : process.env.AUTH_SECRET,
+  session: {
+    enabled: true
+  },
+  jwt: {
+    enabled: false,
+    tokenExpiresIn: '1m',
+    refreshTokenExpiresIn: '7d'
+  },
   password: {
     requireEmailConfirmation: false,
     sendPasswordChangesEmail: false,
