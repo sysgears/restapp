b3ec2b177996d3a4f813d61acd1cb6d112ade788
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-05-15 10:32:07 +0300

    Remove table auth_certificate from migrations

--- modules/user/server-ts/migrations/002_user.js
+++ modules/user/server-ts/migrations/002_user.js
@@ -21,17 +21,6 @@ exports.up = function(knex, Promise) {
         .onDelete('CASCADE');
       table.timestamps(false, true);
     }),
-    knex.schema.createTable('auth_certificate', table => {
-      table.increments();
-      table.string('serial').unique();
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('user')
-        .onDelete('CASCADE');
-      table.timestamps(false, true);
-    }),
     knex.schema.createTable('auth_facebook', table => {
       table.increments();
       table.string('fb_id').unique();
