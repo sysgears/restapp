2205cf2dbd27b088e3e588ea0927882a8fd00a12
Ivan Isakov <washerd2008@gmail.com>
2019-05-29 11:45:32 +0300

    Create migration and seeds for repot module

---
+++ modules/reports/server-ts/sql.ts
@@ -0,0 +1,10 @@
+import { knex } from '@restapp/database-server-ts';
+
+export default class ReportsDAO {
+  public getReportData() {
+    return knex
+      .select('id', 'name', 'phone', 'email')
+      .from('report')
+      .orderBy('id', 'asc');
+  }
+}
