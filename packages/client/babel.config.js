dac6f33ccce9e5ea1a2c43a578a1b4c35a81ac12
Konstantin Stepakh <k.stepah@gmail.com>
2019-06-03 20:13:26 +0300

    Update intro native

--- packages/client/babel.config.js
+++ packages/client/babel.config.js
@@ -4,7 +4,11 @@ module.exports = api => {
   if (isTest) {
     return {
       compact: false,
-      presets: ['@babel/preset-typescript', '@babel/preset-react', ['@babel/preset-env', { modules: 'commonjs' }]],
+      presets: [
+        '@babel/preset-typescript',
+        'module:metro-react-native-babel-preset',
+        ['@babel/preset-env', { modules: 'commonjs' }]
+      ],
       plugins: [
         '@babel/plugin-transform-destructuring',
         '@babel/plugin-transform-regenerator',
@@ -18,15 +22,20 @@ module.exports = api => {
   } else {
     return {
       compact: false,
-      presets: ['@babel/preset-react', ['@babel/preset-env', { modules: false }]],
+      presets: ['module:metro-react-native-babel-preset'],
       plugins: [
-        '@babel/plugin-transform-destructuring',
-        '@babel/plugin-transform-regenerator',
-        '@babel/plugin-transform-runtime',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        '@babel/plugin-proposal-object-rest-spread',
-        ['styled-components', { ssr: true }]
+        'haul/src/utils/fixRequireIssues',
+        ['styled-components', { ssr: true }],
+        ['import', { libraryName: 'antd-mobile' }],
+        [
+          'babel-plugin-module-resolver',
+          {
+            alias: {
+              'react-native-vector-icons': '@expo/vector-icons'
+            }
+          }
+        ],
+        ['@babel/plugin-proposal-decorators', { legacy: true }]
       ],
       env: {
         production: {
