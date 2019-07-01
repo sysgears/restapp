fcba11faf3ce88bc4502783837ef299f8e22def4
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-01 14:21:10 +0300

    Add payment's server module

--- packages/server/src/modules.ts
+++ packages/server/src/modules.ts
@@ -1,13 +1,14 @@
-import welcome from '@restapp/welcome-server-ts';
+import cookies from '@restapp/cookies-server-ts';
 import core from '@restapp/core-server-ts';
 import i18n from '@restapp/i18n-server-ts';
-import validation from '@restapp/validation-common-react';
-import cookies from '@restapp/cookies-server-ts';
 import mailer from '@restapp/mailer-server-ts';
+import payments from '@restapp/payments-server-ts';
+import validation from '@restapp/validation-common-react';
+import welcome from '@restapp/welcome-server-ts';
 import '@restapp/debug-server-ts';
 
 import ServerModule from '@restapp/module-server-ts';
 
-const modules: ServerModule = new ServerModule(welcome, cookies, i18n, validation, mailer, core);
+const modules: ServerModule = new ServerModule(welcome, cookies, i18n, validation, mailer, core, payments);
 
 export default modules;
