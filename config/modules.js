5366e06e05a028d06f6fe56987777fc1ca532965
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-01 11:46:10 +0300

    Create config module

--- config/modules.js
+++ config/modules.js
@@ -1,5 +1,5 @@
+export { default as analytics } from './analytics';
 export { default as app } from './app';
 export { default as db } from './db';
-export { default as mailer } from './mailer';
-export { default as analytics } from './analytics';
 export { default as i18n } from './i18n';
+export { default as mailer } from './mailer';
