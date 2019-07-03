a4d545c0647aa4762b7983722b46f7c5c8c8cf09
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-03 10:34:26 +0300

    Add config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
+export { default as analytics } from './analytics';
 export { default as app } from './app';
 export { default as auth } from './auth';
 export { default as db } from './db';
-export { default as mailer } from './mailer';
-export { default as analytics } from './analytics';
 export { default as i18n } from './i18n';
+export { default as mailer } from './mailer';
+export { default as stripe } from './stripe';
