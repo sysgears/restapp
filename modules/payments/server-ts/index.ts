42f2ebb9bbc9532d4e2e99bdd04c73cf9646c4f3
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-03 10:27:59 +0300

    Create payment module

---
+++ modules/payments/server-ts/index.ts
@@ -0,0 +1,3 @@
+import ServerModule from '@restapp/module-server-ts';
+
+export default new ServerModule();
--- packages/server/src/modules.ts
+++ packages/server/src/modules.ts
@@ -1,15 +1,26 @@
-import welcome from '@restapp/welcome-server-ts';
+import authentication from '@restapp/authentication-server-ts';
+import cookies from '@restapp/cookies-server-ts';
 import core from '@restapp/core-server-ts';
 import i18n from '@restapp/i18n-server-ts';
-import validation from '@restapp/validation-common-react';
-import cookies from '@restapp/cookies-server-ts';
 import mailer from '@restapp/mailer-server-ts';
+import payments from '@restapp/payments-server-ts';
 import user from '@restapp/user-server-ts';
-import authentication from '@restapp/authentication-server-ts';
+import validation from '@restapp/validation-common-react';
+import welcome from '@restapp/welcome-server-ts';
 import '@restapp/debug-server-ts';
 
 import ServerModule from '@restapp/module-server-ts';
 
-const modules: ServerModule = new ServerModule(authentication, welcome, cookies, i18n, validation, mailer, core, user);
+const modules: ServerModule = new ServerModule(
+  authentication,
+  welcome,
+  cookies,
+  i18n,
+  validation,
+  mailer,
+  core,
+  user,
+  payments
+);
 
 export default modules;
