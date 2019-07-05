409324d1de38c1852a5855ba6141492e381f8412
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-05 12:13:47 +0300

    Add subscriber page navItem

--- modules/payments/client-react/stripe/subscription/index.tsx
+++ modules/payments/client-react/stripe/subscription/index.tsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ClientModule from '@restapp/module-client-react';
+import { MenuItem } from '@restapp/look-client-react';
+import { translate, TranslateFunction } from '@restapp/i18n-client-react';
 
-export default new ClientModule();
+const NavLinkWithI18n = translate('stripeSubscription')(({ t }: { t: TranslateFunction }) => (
+  <NavLink to="/subscriber-page" className="nav-link" activeClassName="active">
+    {t('navLink')}
+  </NavLink>
+));
+
+export default new ClientModule({
+  navItem: [
+    <MenuItem key="/subscriber-page">
+      <NavLinkWithI18n />
+    </MenuItem>
+  ]
+});
