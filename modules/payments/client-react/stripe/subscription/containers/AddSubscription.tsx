f1a4c279ffd45f5efd57b1a888e42a929d94d157
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-08 16:56:51 +0300

    Move LoadStripeSDk to a separate component

--- modules/payments/client-react/stripe/subscription/containers/AddSubscription.tsx
+++ modules/payments/client-react/stripe/subscription/containers/AddSubscription.tsx
@@ -1,52 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StripeProvider } from 'react-stripe-elements';
-import { Helmet } from 'react-helmet';
 
 import { translate } from '@restapp/i18n-client-react';
 import { PLATFORM } from '@restapp/core-common';
-import settings from '../../../../../../settings';
+// import { FormError } from '@gqlapp/forms-client-react';
 
-import { AddSubscriptionProps } from '../types';
+import settings from '../../../../../../settings';
+import { AddSubscriptionProps, CreditCardInput } from '../types';
+import LoadStripeSDK from './LoadStripeSDK';
 import AddSubscriptionView from '../components/AddSubscriptionView';
-import { PageLayout } from '@restapp/look-client-react';
+import { createCreditCardToken } from '../stripeOperations';
 
 // react-stripe-elements will not render on the server and on the mobile.
-const AddSubscription = ({ t }: AddSubscriptionProps) => {
+const AddSubscription = ({ t, history, navigation }: AddSubscriptionProps) => {
   const [submitting, setSubmitting] = useState(false);
 
-  /* Stripe elements should render only for web*/
-  return __CLIENT__ && PLATFORM === 'web' ? (
-    <StripeProvider apiKey={settings.stripe.subscription.publicKey}>
-      <AddSubscriptionView submitting={submitting} onSubmit={() => {}} t={t} />
-    </StripeProvider>
-  ) : (
-    <AddSubscriptionView submitting={submitting} onSubmit={() => {}} t={t} />
-  );
-};
+  const onSubmit = async (creditCardInput: CreditCardInput, stripe?: any) => {
+    setSubmitting(true);
 
-const LoadStripeSDK = (props: AddSubscriptionProps) => {
-  const [isLoading, setLoading] = useState(!window.Stripe);
-
-  useEffect(() => {
-    if (isLoading) {
-      const script = document.getElementById('stripe-js');
-      const onLoading = () => {
-        script.removeEventListener('load', onLoading);
-        return setLoading(false);
-      };
-
-      script.addEventListener('load', onLoading);
+    try {
+      const preparedCreditCard = await createCreditCardToken(creditCardInput, stripe);
+      setSubmitting(false);
+      history ? history.push('/subscriber-page') : navigation.goBack();
+    } catch (error) {
+      setSubmitting(false);
     }
-  }, []);
+  };
 
+  /* Stripe elements should render only for web*/
   return (
-    <>
-      <Helmet>
-        <script id="stripe-js" src="https://js.stripe.com/v3/" type="text/javascript" async />
-      </Helmet>
-      <PageLayout>{isLoading ? props.t('loading') : <AddSubscription {...props} />}</PageLayout>
-    </>
+    <LoadStripeSDK t={t}>
+      {__CLIENT__ && PLATFORM === 'web' ? (
+        <StripeProvider apiKey={settings.stripe.subscription.publicKey}>
+          <AddSubscriptionView submitting={submitting} onSubmit={onSubmit} t={t} />
+        </StripeProvider>
+      ) : (
+        <AddSubscriptionView submitting={submitting} onSubmit={onSubmit} t={t} />
+      )}
+    </LoadStripeSDK>
   );
 };
 
-export default translate('stripeSubscription')(LoadStripeSDK);
+export default translate('stripeSubscription')(AddSubscription);
---
+++ modules/payments/client-react/stripe/subscription/containers/LoadStripeSDK.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import Helmet from 'react-helmet';
+
+import { PageLayout } from '@restapp/look-client-react';
+
+const LoadStripeSDK = ({ children, t }: { [key: string]: any }) => {
+  const [isLoading, setLoading] = useState(!window.Stripe);
+
+  useEffect(() => {
+    if (isLoading) {
+      const script = document.getElementById('stripe-js');
+      const onLoading = () => {
+        script.removeEventListener('load', onLoading);
+        return setLoading(false);
+      };
+
+      script.addEventListener('load', onLoading);
+    }
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <script id="stripe-js" src="https://js.stripe.com/v3/" type="text/javascript" async />
+      </Helmet>
+      <PageLayout>{isLoading ? t('loading') : children}</PageLayout>
+    </>
+  );
+};
+
+export default LoadStripeSDK;
