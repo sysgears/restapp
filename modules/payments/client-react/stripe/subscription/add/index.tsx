66b96a05608e0a9f84c484af119a0a8bfe829cbb
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-07-10 16:59:24 +0300

    Change the component

--- modules/payments/client-react/stripe/subscription/add/index.tsx
+++ modules/payments/client-react/stripe/subscription/add/index.tsx
@@ -3,10 +3,10 @@ import ClientModule from '@restapp/module-client-react';
 import { AuthRoute } from '@restapp/user-client-react/containers/Auth';
 import { UserRole } from '@restapp/user-client-react/types';
 
-import AddSubscription from './containers/AddSubscription';
+import Add from './containers/Add';
 import reducer from './reducer';
 
 export default new ClientModule({
-  route: [<AuthRoute exact path="/add-subscription" role={UserRole.user} component={AddSubscription} />],
+  route: [<AuthRoute exact path="/add-subscription" role={UserRole.user} component={Add} />],
   reducer: [{ addSubscription: reducer }]
 });
